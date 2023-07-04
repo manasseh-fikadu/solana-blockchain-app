63204f9eacb8defc108c7f413dd968db618e7c5c
manasseh-fikadu <minase1047@gmail.com>
2023-07-04 12:31:33 +0300

    Initial Commit

---
+++ frontend/context/WalletConnectionProvider.js
@@ -0,0 +1,26 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+
+const connection =
+  "https://patient-divine-pool.solana-devnet.discover.quiknode.pro/8ae6e055b9eb4e7235fd525b711149647ee93951/";
+
+const WalletConnectionProvider = ({ children }) => {
+  const endpoint = useMemo(() => connection, []);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletConnectionProvider;
