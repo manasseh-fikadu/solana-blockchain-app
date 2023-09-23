216433b0125c2ef7c2d66b7a77cd1bfa7f4182c9
manasseh-fikadu <minase1047@gmail.com>
2023-09-24 01:52:28 +0300

    Final Commit

--- frontend/context/WalletConnectionProvider.js
+++ frontend/context/WalletConnectionProvider.js
@@ -1,26 +1,20 @@
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { useMemo } from "react";
-
-const connection =
-  "https://patient-divine-pool.solana-devnet.discover.quiknode.pro/8ae6e055b9eb4e7235fd525b711149647ee93951/";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { useMemo } from 'react'
 
 const WalletConnectionProvider = ({ children }) => {
-  const endpoint = useMemo(() => connection, []);
+    const endpoint = useMemo(() => 'https://api.devnet.solana.com', [])
 
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
 
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>{children}</WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
-};
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
 
-export default WalletConnectionProvider;
+export default WalletConnectionProvider
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,25 +1,22 @@
-import dynamic from "next/dynamic";
-import Head from "next/head";
-import "../styles/globals.css";
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import '../styles/globals.css'
 
-const WalletConnectionProvider = dynamic(
-  () => import("../context/WalletConnectionProvider"),
-  {
+const WalletConnectionProvider = dynamic(() => import('../context/WalletConnectionProvider'), {
     ssr: false,
-  }
-);
+})
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Airbnb Clone</title>
-      </Head>
-      <WalletConnectionProvider>
-        <Component {...pageProps} />
-      </WalletConnectionProvider>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Airbnb Clone</title>
+            </Head>
+            <WalletConnectionProvider>
+                <Component {...pageProps} />
+            </WalletConnectionProvider>
+        </>
+    )
 }
 
-export default MyApp;
+export default MyApp
